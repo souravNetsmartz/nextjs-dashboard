b9a28b46665eac2c895eec773d93567dfdf4e9bb
“souravNetsmartz” <sourav.sharma@netsmartz.net>
2023-10-31 16:44:31 +0530

    code

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Invoice } from "../../lib/definitions";
+
+export default function Invoice() {
+  return (
+    <div>
+      <p>Customers Page</p>
+    </div>
+  );
+}
